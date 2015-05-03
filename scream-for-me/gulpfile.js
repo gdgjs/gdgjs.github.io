dc3714222dd37d0b6002705eaf4c5cf0f66acbaa
Lucas <lucas@agtlucas.com>
2015-05-03 08:13:45 -0300

    Update gulpfile

--- scream-for-me/gulpfile.js
+++ scream-for-me/gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var minifycss = require('gulp-minify-css');
+var neat = require('node-neat').includePaths;
+var transform = require('vinyl-transform');
 
 // Browserify task
 gulp.task('browserify', function() {
